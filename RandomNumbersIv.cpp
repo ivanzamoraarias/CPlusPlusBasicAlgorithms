073508589edd5286853118b8b0c3c90c1d76d706
ivanzamoraarias <faucar93@gmail.com>
2021-09-25 01:13:47 -0500

    Randon Linear base class generator

---
+++ RandomNumbersIv.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+using namespace std;
+
+
+
+class LinearRandomNumbers
+{
+private:
+    int m;
+    int a;
+    int c;
+    int Xo;
+
+
+public:
+    LinearRandomNumbers(
+        int Modulus, 
+        int Multiplier, 
+        int Increment, 
+        int StartingValue
+    );
+    ~LinearRandomNumbers();
+
+    int GetRandom();
+    int GetLinearRandom();
+
+private:
+    int GetModuloSum(int x , int y, int m);
+};
+
+LinearRandomNumbers::LinearRandomNumbers(    
+        int Modulus, 
+        int Multiplier, 
+        int Increment, 
+        int StartingValue
+    )
+{
+     m = Modulus;
+     a = Multiplier;
+     c = Increment;
+     Xo = StartingValue;
+
+}
+
+LinearRandomNumbers::~LinearRandomNumbers()
+{
+}
+
+int LinearRandomNumbers::GetRandom()
+{
+    int x = Xo;
+    int r = (a * x) % m;
+    
+    r = GetModuloSum(r, c, m);
+    x = r;
+
+    return x;
+}
+
+int LinearRandomNumbers::GetLinearRandom()
+{
+    int x = Xo;
+    int q = m / a;
+    int p = m % a;
+    int r = a * (x % q) - p*(x/q);
+
+    if( r < 0 )
+        r = r + m;
+
+     r = GetModuloSum(r, c, m);
+     x = r;
+
+     return r;
+}
+
+int LinearRandomNumbers::GetModuloSum(int x , int y, int m)
+{
+    if(x <= (m-1-y))
+    {
+        return x + y;
+    }
+
+    return x - (m - y);
+
+}
+
+
+int main()
+{
+    LinearRandomNumbers* LinearRand = 
+        new LinearRandomNumbers(
+            10,
+            8, 
+            7,
+            9
+        );
+
+    LinearRand->GetRandom();
+    LinearRand->GetLinearRandom();
+
+    free(LinearRand);
+}
